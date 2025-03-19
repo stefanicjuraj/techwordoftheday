f9a52afc4211fee49b6ac110595179e6c1006724
Juraj Stefanic <jurajstefanic@outlook.com>
2025-03-19 15:01:15 +0100

    feat: add layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,23 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "",
-  description: "",
+  title: "Tech Word of the Day",
+  description: "Daily tech word with definition.",
+  keywords: ["Tech Word of the Day", "Tech Words", "Word of the Day"],
+  openGraph: {
+    title: "Tech Word of the Day",
+    description: "Daily tech word with definition.",
+    url: "",
+    siteName: "Tech Word of the Day",
+    images: [
+      {
+        url: "",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -13,6 +28,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/assets/icons/techwordoftheday.svg" />
+      </head>
       <body>{children}</body>
     </html>
   );
