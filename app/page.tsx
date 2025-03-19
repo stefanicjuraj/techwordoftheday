1a2500d767a86e6f2ee9d8b1a2dc7a33a713a708
Juraj Stefanic <jurajstefanic@outlook.com>
2025-03-19 15:00:34 +0100

    feat: add techwordoftheday component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import TechWordOfTheDay from "@/components/TechWordOfTheDay";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-6">
+      <h1 className="text-7xl text-white font-bold mb-10">
+        Tech Word of the Day
+      </h1>
+      <TechWordOfTheDay />
+    </div>
+  );
 }
